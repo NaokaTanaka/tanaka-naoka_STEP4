4841c05272d308818d01347af2e90b9c03f1c132
NaokaTanaka <n.tanaka.official@gmail.com>
2026-01-05 18:48:08 +0900

    second commit

--- src/conform.php
+++ src/conform.php
@@ -2,34 +2,54 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <title>Form</title>
-    <link rel="stylesheet" href="style.css">
+    <title>入力内容確認</title>
 </head>
 <body>
   <h1>入力内容確認</h1>
     <?php
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-      $name = $_POST['name'];
-      $age = $_POST['age'];
-      $tel = $_POST['tel'];
-      $email = $_POST['email'];
-      $address = $_POST['address'];
-      $question = $_POST['question'];
-      $gender = $_POST['gender'];
+    if($_SERVER["REQUEST_METHOD"] === "POST"){
+      $name = $_POST["name"];
+      $age = $_POST["age"];
+      $tel = $_POST["tel"];
+      $email = $_POST["email"];
+      $address = $_POST["address"];
+      $question = $_POST["question"];
+      $selectedValue = $_POST["gender"];
+      $gender = [
+        "male" => "男性",
+        "female" => "女性"
+      ];
 
-      if(!preg_match('/^[ぁ-んァ-ヶーー-龠a-zA-Z\s]+$/u', $name)){
+        if(!preg_match('/^[ぁ-んァ-ヶーー-龠a-zA-Z\s]+$/u', $name)) {
         echo "<p>ひらがな、カタカナ、漢字、英字のみで入力してください。</p>";
-      } elseif (!preg_match('/^[0-9]{0,150}+$/', $age)){
+        }
+        if (!is_numeric($age) || $age < 0 || $age > 150) {
         echo "<p>0から150の半角数字で入力してください。</p>";
-      } elseif (!preg_match('/^[0-9-]+$/', $tel)){
+        }
+        if (!preg_match('/^[0-9-]+$/', $tel)) {
         echo "<p>半角数字とハイフンのみで入力してください。</p>";
-      } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        }
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
         echo "<p>メールアドレスの形式で入力してください。</p>";
-      } elseif (!preg_match('/^[ぁ-んァ-ヶーー-龠a-zA-Z\s]+$/u', $address)){
+        }
+        if (!preg_match('/^[ぁ-んァ-ヶーー-龠a-zA-Z\s]+$/u', $address)) {
+          echo "<p>質問を入力してください</p>";
+        }
+        if (!preg_match('/\S/', $question)) {
         echo "<p>ひらがな、カタカナ、漢字、英字のみで入力してください。</p>";
+        } else {
+          // 入力内容表示
+          echo "<p>名前:".htmlspecialchars($name,ENT_QUOTES,'UTF-8')."</p>";
+          echo "<p>年齢:{$age}</p>";
+          echo "<p>電話番号:{$tel}</p>";
+          echo "<p>メールアドレス:".htmlspecialchars($email,ENT_QUOTES,'UTF-8')."</p>";
+          echo "<p>住所:".htmlspecialchars($address,ENT_QUOTES,'UTF-8')."</p>";
+          echo "<p>質問:".htmlspecialchars($question,ENT_QUOTES,'UTF-8')."</p>";
+          $selectedLabel = $gender[$selectedValue];
+          echo "<p>性別:".htmlspecialchars($selectedLabel,ENT_QUOTES,'UTF-8')."</p>";
+        }
       } else {
-      echo "<p>入力内容を確認してください。</p>";
-      }
+      echo "<p>データが送信されていません。</p>";
     }
     ?>
 </body>
